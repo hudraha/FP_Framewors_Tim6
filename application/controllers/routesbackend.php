32265229728556c679ed78c56dc2bf5c76465c80
hudraha <54569636+hudraha@users.noreply.github.com>
2021-05-26 22:41:16 +0700

    Delete application directory

--- application/controllers/routesbackend.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class routesbackend extends CI_Controller {
-	public function index()
-	{
-		redirect(base_url('backend'));
-	}
-}
